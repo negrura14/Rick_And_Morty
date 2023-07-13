015cfc25194b336512cbf5951acf23c9fe1a1257
negrura14 <talleresmatador114@gmail.com>
2023-07-13 20:24:48 -0300

    DB

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,12 @@
 const PORT = 3001;
 const server = require('./app')
+const { conn } = require("./DB_connection")
 
-server.listen(PORT, ()=>{
-    console.log('Server raised in port:' + PORT);
+conn.sync({force: false}).then(()=>{
+
+    server.listen(PORT, ()=>{
+        console.log('Server raised in port:' + PORT);
+    })
+}).catch((error) =>{
+    console.log(error);
 })
